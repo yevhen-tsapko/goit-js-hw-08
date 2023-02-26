80f4d9325f1df05c290eaede7c71a3b9d4cc99b6
yevhen-tsapko <111687103+yevhen-tsapko@users.noreply.github.com>
2023-02-26 12:46:10 +0000

    Deploying to gh-pages from @ yevhen-tsapko/goit-js-hw-08@2b575a2bdf997f2db0c8121946f25170cec66c3e 🚀

--- 03-feedback.e4f5b59a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(e=>{const t={email:e.currentTarget.elements.email.value,message:e.currentTarget.elements.message.value};console.log(t),localStorage.setItem("form-data",JSON.stringify(t))}));
-//# sourceMappingURL=03-feedback.e4f5b59a.js.map
