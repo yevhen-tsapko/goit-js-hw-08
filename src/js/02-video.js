2b575a2bdf997f2db0c8121946f25170cec66c3e
Yevhen Tsapko <evgentsa@gmail.com>
2023-02-26 14:45:39 +0200

    All tasks are made

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,7 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-console.log(throttle);
+
 if (localStorage.getItem('videoplayer-current-time')) {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 }
@@ -12,7 +12,5 @@ player.on(
   'timeupdate',
   throttle(({ seconds }) => {
     localStorage.setItem('videoplayer-current-time', seconds);
-    console.log(seconds);
   }, 1000)
 );
-console.log('jhgkjhgkjhg', localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,31 @@
+import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', evt => {
-  const formDatas = {
-    email: evt.currentTarget.elements.email.value,
-    message: evt.currentTarget.elements.message.value,
-  };
-  console.log(formDatas);
-  localStorage.setItem('form-data', JSON.stringify(formDatas));
-});
+if (localStorage.getItem('feedback-form-state')) {
+  const { email, message } = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  form.elements.email.value = email;
+  form.elements.message.value = message;
+}
+form.addEventListener(
+  'input',
+  throttle(
+    evt => {
+      const formDatas = {
+        email: evt.currentTarget.elements.email.value,
+        message: evt.currentTarget.elements.message.value,
+      };
+      localStorage.setItem('feedback-form-state', JSON.stringify(formDatas));
+    },
+    500,
+    { trailing: false }
+  )
+);
 
-// form.addEventListener('submit', {});
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.table(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  evt.currentTarget.reset();
+});
